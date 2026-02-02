41f4a79d9e04f0746c11c64db31fefe9d25d41b6
Antonio-M. Corbi Bellot <antonio.corbi@gmail.com>
2026-02-02 20:09:22 +0100

    More updates.

---
+++ src/app.rs
@@ -0,0 +1,42 @@
+// Copyright (C) 2026  Antonio-M. Corbi Bellot
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+pub struct App3D {
+    rotx: bool,
+    roty: bool,
+    rotz: bool,
+    zoom: f32,
+}
+
+impl App3D {
+    pub fn new() -> Self {
+        Self {
+            rotx: true,
+            roty: false,
+            rotz: false,
+            zoom: 1.0,
+        }
+    }
+}
+
+impl eframe::App for App3D {
+    /// Called by the framework to save state before shutdown.
+    // fn save(&mut self, storage: &mut dyn eframe::Storage) {
+    //     eframe::set_value(storage, eframe::APP_KEY, self);
+    // }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {}
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,41 @@
+mod app;
 mod penger;
 mod types;
 
-fn main() {
-    let l: types::Line;
-    let ll: types::Lines;
-    let p: types::Point3D;
+// fn main() {
+//     let l: types::Line;
+//     let ll: types::Lines;
+//     let p: types::Point3D;
+//
+//     println!("point3d[300]  = {:?}", crate::penger::VS[300]);
+//     println!("line[300]  = {:?}", crate::penger::FS[300]);
+// }
 
-    println!("point3d[300]  = {:?}", crate::penger::VS[300]);
-    println!("line[300]  = {:?}", crate::penger::FS[300]);
+// -- Native App: ---------------------------------------------------------
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    //env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+
+    let native_options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_app_id("Floater") // niri uses this app_id to make the window floating
+            .with_inner_size([1024.0, 768.0])
+            .with_min_inner_size([500.0, 300.0])
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+                    .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Penger3D GUI",
+        native_options,
+        Box::new(|cc| {
+            let cc = cc;
+            Ok(Box::new(app::App3D::new()))
+        }),
+    )
 }
